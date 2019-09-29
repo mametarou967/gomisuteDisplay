ca606fb0f2ba807c4a6022abc337585d5162faa2
tarou mame <mametarou963@outlook.jp>
2019-09-29 17:32:37 +0900

    リファクタリング

--- src/gomisuteDisplay/gomisuteDisplay.ino
+++ src/gomisuteDisplay/gomisuteDisplay.ino
@@ -1,16 +1,18 @@
 #include <M5Stack.h>
 
+// common window
+#define ORGPOINTX 10
+
 // main window offset
-#define MAIN_TEXT_OFFSETY  0
-#define MAIN_PICTURE_OFFSETY 24
-#define MAIN1_OFFSETX  10
-#define MAIN2_OFFSETX  160
+#define MAIN_TEXT_ORGPOINTY  0
+#define MAIN_PICTURE_ORGPOINTY 24
+#define MAIN_OFFSETX  150
+#define MAIN_INDEX_MAX 2
 
 // sub window offset
-#define SUB_TEXT_ORGPOINTY 184
-#define SUB_PICTURE_ORGPOINTY 200
-#define ORGPOINTX 10
-#define SUB_OFFSETX 50 
+#define SUB_TEXT_ORGPOINTY 174
+#define SUB_PICTURE_ORGPOINTY 190
+#define SUB_OFFSETX 50
 #define SUB_INDEX_MAX 6
 
 
@@ -22,26 +24,19 @@ void printPt16(const char* buf,uint16_t x, uint16_t y, int color){
   M5.Lcd.unloadFont();
 }
 
-void printMain1Text(const char* buf){
-  printPt16(buf,MAIN1_OFFSETX,MAIN_TEXT_OFFSETY,BLACK);
-}
-
-void printMain1Picture(const char* fileName){
-  M5.Lcd.drawJpgFile(SD, fileName,MAIN1_OFFSETX,MAIN_PICTURE_OFFSETY);
-}
-
-void printMain2Text(const char* buf){
-  printPt16(buf,MAIN2_OFFSETX,MAIN_TEXT_OFFSETY,BLACK);
-}
-
-void printMain2Picture(const char* fileName){
-  M5.Lcd.drawJpgFile(SD, fileName,MAIN2_OFFSETX,MAIN_PICTURE_OFFSETY);
-}
-
-
-void printSubText(const char* buf,int index){
+void printMainText(const char* buf,int color,int index){
   if(index >= SUB_INDEX_MAX)  return;
-  printPt16(buf,ORGPOINTX + (SUB_OFFSETX * index),SUB_TEXT_ORGPOINTY,BLACK);
+  printPt16(buf,ORGPOINTX + (MAIN_OFFSETX * index),MAIN_TEXT_ORGPOINTY,color);
+}
+
+void printMainPicture(const char* fileName,int index){
+  if(index >= MAIN_INDEX_MAX)  return;
+  M5.Lcd.drawJpgFile(SD, fileName,ORGPOINTX + (MAIN_OFFSETX * index),MAIN_PICTURE_ORGPOINTY);
+}
+
+void printSubText(const char* buf,int color,int index){
+  if(index >= SUB_INDEX_MAX)  return;
+  printPt16(buf,17/* 即値オフセット*/ + ORGPOINTX + (SUB_OFFSETX * index),SUB_TEXT_ORGPOINTY,color);
 }
 
 void printSubPicture(const char* fileName,int index){
@@ -55,22 +50,22 @@ void setup() {
   M5.begin();
   M5.Lcd.clear(WHITE);
   
-  printMain1Text("本日 10/30(月)");
-  printMain1Picture("/pic150x150/moeru.jpg");
-  printMain2Text("明日 10/31(火)");
-  printMain2Picture("/pic150x150/can.jpg");
+  printMainText("本日 10/30(月)",BLACK,0);
+  printMainPicture("/pic150x150/moeru.jpg",0);
+  printMainText("明日 10/31(火)",BLACK,1);
+  printMainPicture("/pic150x150/can.jpg",1);
   // Sub
-  printSubText("11/1(水)",0);
+  printSubText("水",BLACK,0);
   printSubPicture("/pic50x50/moenai.jpg",0);
-  printSubText("11/1(木)",1);
+  printSubText("木",BLACK,1);
   printSubPicture("/pic50x50/petbottle.jpg",1);
-  printSubText("11/1(金)",2);
+  printSubText("金",BLACK,2);
   printSubPicture("/pic50x50/plastic.jpg",2);
-  printSubText("11/1(土)",3);
+  printSubText("土",BLUE,3);
   printSubPicture("/pic50x50/shigen.jpg",3);
-  printSubText("11/1(日)",4);
+  printSubText("日",RED,4);
   printSubPicture("/pic50x50/sonohoka.jpg",4);
-  printSubText("11/1(月)",5);
+  printSubText("月",BLACK,5);
   printSubPicture("/pic50x50/blank.jpg",5);
 }
 
