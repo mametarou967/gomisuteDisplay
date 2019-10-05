e254eebc44e0cf950486ced07d5d3b77d66e1e22
tarou mame <mametarou963@outlook.jp>
2019-10-05 22:49:35 +0900

    ファイル分割

---
+++ src/gomisuteDisplay/display.ino
@@ -0,0 +1,42 @@
+// common window
+#define ORGPOINTX 10
+
+// main window offset
+#define MAIN_TEXT_ORGPOINTY  0
+#define MAIN_PICTURE_ORGPOINTY 24
+#define MAIN_OFFSETX  150
+#define MAIN_INDEX_MAX 2
+
+// sub window offset
+#define SUB_TEXT_ORGPOINTY 174
+#define SUB_PICTURE_ORGPOINTY 190
+#define SUB_OFFSETX 50
+#define SUB_INDEX_MAX 6
+
+void printPt16(const char* buf,uint16_t x, uint16_t y, int color){
+  M5.Lcd.setTextColor(color);
+  M5.Lcd.loadFont("font/genshin-regular-16pt", SD);
+  M5.Lcd.setCursor(x, y);
+  M5.Lcd.printf(buf);
+  M5.Lcd.unloadFont();
+}
+
+void printMainText(const char* buf,int color,int index){
+  if(index >= SUB_INDEX_MAX)  return;
+  printPt16(buf,ORGPOINTX + (MAIN_OFFSETX * index),MAIN_TEXT_ORGPOINTY,color);
+}
+
+void printMainPicture(const char* fileName,int index){
+  if(index >= MAIN_INDEX_MAX)  return;
+  M5.Lcd.drawJpgFile(SD, fileName,ORGPOINTX + (MAIN_OFFSETX * index),MAIN_PICTURE_ORGPOINTY);
+}
+
+void printSubText(const char* buf,int color,int index){
+  if(index >= SUB_INDEX_MAX)  return;
+  printPt16(buf,17/* 即値オフセット*/ + ORGPOINTX + (SUB_OFFSETX * index),SUB_TEXT_ORGPOINTY,color);
+}
+
+void printSubPicture(const char* fileName,int index){
+  if(index >= SUB_INDEX_MAX)  return;
+  M5.Lcd.drawJpgFile(SD, fileName,ORGPOINTX + (SUB_OFFSETX * index),SUB_PICTURE_ORGPOINTY);
+}
--- src/gomisuteDisplay/gomisuteDisplay.ino
+++ src/gomisuteDisplay/gomisuteDisplay.ino
@@ -2,51 +2,6 @@
 
 char csvBuffer[400][3][10] = {0};
 
-// common window
-#define ORGPOINTX 10
-
-// main window offset
-#define MAIN_TEXT_ORGPOINTY  0
-#define MAIN_PICTURE_ORGPOINTY 24
-#define MAIN_OFFSETX  150
-#define MAIN_INDEX_MAX 2
-
-// sub window offset
-#define SUB_TEXT_ORGPOINTY 174
-#define SUB_PICTURE_ORGPOINTY 190
-#define SUB_OFFSETX 50
-#define SUB_INDEX_MAX 6
-
-
-void printPt16(const char* buf,uint16_t x, uint16_t y, int color){
-  M5.Lcd.setTextColor(color);
-  M5.Lcd.loadFont("font/genshin-regular-16pt", SD);
-  M5.Lcd.setCursor(x, y);
-  M5.Lcd.printf(buf);
-  M5.Lcd.unloadFont();
-}
-
-void printMainText(const char* buf,int color,int index){
-  if(index >= SUB_INDEX_MAX)  return;
-  printPt16(buf,ORGPOINTX + (MAIN_OFFSETX * index),MAIN_TEXT_ORGPOINTY,color);
-}
-
-void printMainPicture(const char* fileName,int index){
-  if(index >= MAIN_INDEX_MAX)  return;
-  M5.Lcd.drawJpgFile(SD, fileName,ORGPOINTX + (MAIN_OFFSETX * index),MAIN_PICTURE_ORGPOINTY);
-}
-
-void printSubText(const char* buf,int color,int index){
-  if(index >= SUB_INDEX_MAX)  return;
-  printPt16(buf,17/* 即値オフセット*/ + ORGPOINTX + (SUB_OFFSETX * index),SUB_TEXT_ORGPOINTY,color);
-}
-
-void printSubPicture(const char* fileName,int index){
-  if(index >= SUB_INDEX_MAX)  return;
-  M5.Lcd.drawJpgFile(SD, fileName,ORGPOINTX + (SUB_OFFSETX * index),SUB_PICTURE_ORGPOINTY);
-}
-
-
 // 最終的にはnumberなんかはenumにしたい
 // bufferも併せてclass化できると良い
 
@@ -125,6 +80,7 @@ void setup() {
   loadGomiListFromCSV("/test.csv",3);
   Serial.println("hello2");
 
+  /*
   for(int k = 0;k < 20;k++){
     Serial.printf("%d----",k);
     
@@ -142,6 +98,7 @@ void setup() {
     
     // printPt16(csvBuffer[k][2],0,k * 16,BLACK);
   }
+  */
   /*
   for(int i = 0;i < 38;i++){
     buf[0] = file.read();
@@ -151,16 +108,7 @@ void setup() {
   // buf[10] = '\n';
   // M5.Lcd.print(buf);
   Serial.println("end");
-  /*
-  if(connma_count%2==0 && connma_count>2){
-    value[buf_number] = buf[0];
-    buf_number++;
-  }
-   if (buf[0] == ',') {
-    connma_count++;
-  }
-  */
-  /*
+  
   printMainText("本日 10/30(月)",BLACK,0);
   printMainPicture("/pic150x150/moeru.jpg",0);
   printMainText("明日 10/31(火)",BLACK,1);
@@ -178,7 +126,6 @@ void setup() {
   printSubPicture("/pic50x50/sonohoka.jpg",4);
   printSubText("月",BLACK,5);
   printSubPicture("/pic50x50/blank.jpg",5);
-  */
 }
 
 void loop() {
